ccebd4eebae8f4fd1b3966276b1b16b2d9eb09f4
teugoes <teugoes@hotmail.com>
2023-04-04 15:07:03 -0300

    Adiciona novos exercícios

---
+++ exercicios-loops-e-arrays/src/ParImpar.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class ParImpar {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int quantNumeros;
+        int numero;
+        int quantPares = 0, quantImpares = 0;
+
+        System.out.println("Quantidade de numeros: ");
+        quantNumeros = scan.nextInt();
+        
+
+        int count = 0;
+        do {
+            System.out.println("Número: ");
+            numero = scan.nextInt();
+
+            if(numero % 2 == 0){
+                quantPares++;
+            }
+            else quantImpares++;
+
+            count++;
+        } while (count < quantNumeros);
+
+        System.out.println("Quantidade Par: " + quantPares);
+        System.out.println("Quantidade Ímpar: " + quantImpares);
+
+        scan.close();
+    }
+}
