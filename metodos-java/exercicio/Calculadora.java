171a7484da179acd6d40b6378ebbadf35216fcc2
teugoes <teugoes@hotmail.com>
2023-05-11 14:54:00 -0300

    Adiciona metodo calculadora funcionando

---
+++ metodos-java/exercicio/Calculadora.java
@@ -0,0 +1,27 @@
+package exercicio;
+
+public class Calculadora {
+    public static void soma(double numero1, double numero2){
+        double resultado = numero1 + numero2;
+
+        System.out.println("A soma de " + numero1 + " + " + numero2 + " é: " + resultado);
+    }
+
+    public static void subtracao(double numero1, double numero2){
+        double resultado = numero1 - numero2;
+
+        System.out.println("A subtracao de " + numero1 + " - " + numero2 + " é: " + resultado);
+    }
+
+    public static void multiplicacao(double numero1, double numero2){
+        double resultado = numero1 * numero2;
+
+        System.out.println("A multiplicacao de " + numero1 + " * " + numero2 + " é: " + resultado);
+    }
+
+    public static void divisao(double numero1, double numero2){
+        double resultado = numero1 / numero2;
+
+        System.out.println("A divisao de " + numero1 + " / " + numero2 + " é: " + resultado);
+    }
+}
