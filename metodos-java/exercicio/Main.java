6f1e674443e0ac446dfa18f266292320e47c3107
teugoes <teugoes@hotmail.com>
2023-05-11 14:52:49 -0300

    Metodo da calculadora funcionando

---
+++ metodos-java/exercicio/Main.java
@@ -0,0 +1,19 @@
+package exercicio;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Calculadora
+        System.out.println("Exercicio Calculadora:");
+
+        Calculadora.soma(3,6);
+        Calculadora.subtracao(9,1.8);
+        Calculadora.multiplicacao(7,8);
+        Calculadora.divisao(5,2.5);
+
+        
+
+        
+
+    }
+}
